e177a52ffdf60afe85f97e5556aaf0b34e318fbb
KristiyanBelev <krisk0@abv.bg>
2015-09-08 01:59:21 +0300

    Added part of the homework for dictionares and hash tables

---
+++ Data-Structures-and-Algorithms/Data-Structures-June-2015/6. Dictionaries-and-Hash-Tables/Dictionaries-and-Hash-Tables/Phonebook/Phonebook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    class Phonebook
+    {
+        static void Main()
+        {
+            var phonebook = new Dictionary<string, string>();
+            var input = "";
+            bool isSearching = false;
+            // Adding the contacts
+            while (!isSearching)
+            {
+                input = Console.ReadLine();
+                if (input != "search")
+                {
+                    var entry = input.Split('-');
+                    phonebook.Add(entry[0].ToString(), entry[1].ToString());
+                }
+                else
+                {
+                    isSearching = true;
+                }
+            }
+
+            // Reading searched entries
+            bool isFinished = false;
+            var searchedEntries = new List<string>();
+            while (!isFinished)
+            {
+                input = Console.ReadLine();
+                if (input == "")
+                {
+                    isFinished = true;
+                }
+                else
+                {
+                    searchedEntries.Add(input);
+                }
+            }
+
+            // Printing the output
+            foreach (var key in searchedEntries)
+            {
+                if (phonebook.ContainsKey(key))
+                {
+                    Console.WriteLine("{0} -> {1}", key, phonebook[key]);
+                }
+                else
+                {
+                    Console.WriteLine("Contact {0} does not exist.", key);
+                }
+            }
+        }
+    }
+}
